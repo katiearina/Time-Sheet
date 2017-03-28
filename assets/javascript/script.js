ce66ca2086b895f99f071542815a1ea3ad31a2ba
_apj_ <adrian.jensen@gmail.com>
2017-03-28 19:58:51 -0400

    Added Functions

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,30 +1,47 @@
 //---------------------------------------------------------------------------
 // VARIABLE DECLARATIONS!
-	var name = $("#name").val().trim();
-	var role = $("#role").val().trim();
-	var startDate = $("#start-date").val().trim();
-	var monthlyRate = $("#monthly-rate").val().trim();
+var name = $("#name").val().trim();
+var role = $("#role").val().trim();
+var startDate = $("#start-date").val().trim();
+var monthlyRate = $("#monthly-rate").val().trim();
+var UTCDate = "";
 
-	// Initialize Firebase
-	var config = {
-		apiKey: "AIzaSyD4OzijyxwS7AwrzydBG4HalHoBjwajV3c",
-		authDomain: "timesheet-95d18.firebaseapp.com",
-		databaseURL: "https://timesheet-95d18.firebaseio.com",
-		storageBucket: "timesheet-95d18.appspot.com",
-		messagingSenderId: "392702487619"
-	};
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyD4OzijyxwS7AwrzydBG4HalHoBjwajV3c",
+    authDomain: "timesheet-95d18.firebaseapp.com",
+    databaseURL: "https://timesheet-95d18.firebaseio.com",
+    storageBucket: "timesheet-95d18.appspot.com",
+    messagingSenderId: "392702487619"
+};
 
-	firebase.initializeApp(config);
+firebase.initializeApp(config);
 
 //---------------------------------------------------------------------------
 // FUNCTION DECLARATIONS!
-function createRow {
+function createRow() {
+
+}
+
+function getBill(rate, months) {
+
+}
+
+function getMonthsWorked(UTCDate) {
+
+}
+
+function convertDatetoUTC(startDate) {
+
+}
+
+function convertDatefromUTC(UTCDate) {
 
 }
 
 //---------------------------------------------------------------------------
 // DO STUFF!
-$("#submit-button").on("click", function(event){
-	event.preventDefault();
-	createRow();
+$("#submit-button").on("click", function(event) {
+    event.preventDefault();
+    createRow();
 });
