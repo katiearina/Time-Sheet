1f98183ec30c0292ed22e72bd4e4c1984643fe3b
Katie <katiearina@gmail.com>
2017-03-28 19:49:21 -0400

    Added basic JS. Need to create table in HTML file

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,14 +1,30 @@
 //---------------------------------------------------------------------------
 // VARIABLE DECLARATIONS!
+	var name = $("#name").val().trim();
+	var role = $("#role").val().trim();
+	var startDate = $("#start-date").val().trim();
+	var monthlyRate = $("#monthly-rate").val().trim();
 
+	// Initialize Firebase
+	var config = {
+		apiKey: "AIzaSyD4OzijyxwS7AwrzydBG4HalHoBjwajV3c",
+		authDomain: "timesheet-95d18.firebaseapp.com",
+		databaseURL: "https://timesheet-95d18.firebaseio.com",
+		storageBucket: "timesheet-95d18.appspot.com",
+		messagingSenderId: "392702487619"
+	};
 
-
+	firebase.initializeApp(config);
 
 //---------------------------------------------------------------------------
 // FUNCTION DECLARATIONS!
+function createRow {
 
-
-
+}
 
 //---------------------------------------------------------------------------
 // DO STUFF!
+$("#submit-button").on("click", function(event){
+	event.preventDefault();
+	createRow();
+});
